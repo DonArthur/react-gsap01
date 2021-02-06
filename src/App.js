5236dfa4c8e650aae7cadc81fdf673c79f307372
Geraldy <geraldy.miero@gmail.com>
2021-02-07 02:17:54 +0700

    add router and styling

--- src/App.js
+++ src/App.js
@@ -1,55 +1,34 @@
-import React, { useRef, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { TweenMax, Power3 } from 'gsap'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home'
+import Second from './pages/Second'
 
 function App() {
-  let logoItem = useRef(null)
-  let textItem = useRef(null)
-
-  useEffect(() => {
-    TweenMax.to(
-      logoItem,
-      1.5,
-      {
-        opacity: 1,
-        y: -50,
-        ease: Power3.easeOut
-      }
-    )
-    TweenMax.to(
-      textItem,
-      1.5,
-      {
-        opacity: 1,
-        x: -50,
-        ease: Power3.easeOut
-      }
-    )
-  },[])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          ref={el => {logoItem = el}} 
-          src={logo} 
-          className="App-logo" 
-          alt="logo" 
-        />
-        <p className="text-title" ref={el => {textItem = el}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <div>
+          <nav className="navbar">
+            <ul>
+              <li>
+                <Link className="navbar-text" to="/">Home</Link>
+              </li>
+              <li>
+                <Link className="navbar-text" to="/second-tutorial">Second Tutorial</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/second-tutorial">
+              <Second />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </header>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import React, { useRef, useEffect } from 'react'
+import logo from '../assets/images/man-united.png';
+import '../App.css';
+import { TweenMax, Power3 } from 'gsap'
+
+const Home = () => {
+    let logoItem = useRef(null)
+    let textItem = useRef(null)
+    let textItemLeft = useRef(null)
+  
+    useEffect(() => {
+      TweenMax.to(
+        logoItem,
+        1.5,
+        {
+          opacity: 1,
+          x: 50,
+          ease: Power3.easeOut
+        }
+      )
+      TweenMax.to(
+        textItem,
+        1.5,
+        {
+          opacity: 1,
+          y: 20,
+          ease: Power3.easeOut
+        }
+      )
+      TweenMax.to(
+        textItemLeft,
+        1.5,
+        {
+          opacity: 1,
+          y: 20,
+          ease: Power3.easeOut
+        }
+      )
+    },[])
+      
+    return (
+        <div className="container">
+            <div>
+                <p className="text-title" ref={el => {textItemLeft = el}}>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+            <div>
+                <img
+                    ref={el => {logoItem = el}} 
+                    src={logo} 
+                    className="App-logo" 
+                    alt="logo" 
+                />
+            </div>
+            <div>
+                <p className="text-title" ref={el => {textItem = el}}>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
